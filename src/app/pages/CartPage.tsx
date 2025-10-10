b19528b0ed78e849e12a4bc8734abb02b68dc505
Erick Verde <erick.verde@unmsm.edu.pe>
2025-10-10 17:03:24 -0500

    commit : Resolve Labo 26

--- src/app/pages/CartPage.tsx
+++
@@ -1,5 +0,0 @@
-import { useMemo } from 'react';
-import { useCart } from '../context/cart/CartContext';
-import { formatPrice } from '../utils/price';
-import CartItemActions from '../components/CartItemActions'; 
-import { ShoppingCart } from 'lucide-react';
